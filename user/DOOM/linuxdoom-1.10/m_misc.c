9f36feb9ac0d3b59d03a22eacc1d6c092f2e684e
Ali Smadi <alielsmadi@gmail.com>
2026-03-13 23:49:16 +1100

    Fix keyboard bindings for doom gameplay

--- user/DOOM/linuxdoom-1.10/m_misc.c
+++ user/DOOM/linuxdoom-1.10/m_misc.c
@@ -239,7 +239,6 @@ default_t	defaults[] =
     {"show_messages",&showMessages, 1},
     
 
-#ifdef NORMALUNIX
     {"key_right",&key_right, KEY_RIGHTARROW},
     {"key_left",&key_left, KEY_LEFTARROW},
     {"key_up",&key_up, KEY_UPARROW},
@@ -252,12 +251,9 @@ default_t	defaults[] =
     {"key_strafe",&key_strafe, KEY_RALT},
     {"key_speed",&key_speed, KEY_RSHIFT},
 
-// UNIX hack, to be removed. 
 #ifdef SNDSERV
     {"sndserver", (int *) &sndserver_filename, (int) "sndserver"},
     {"mb_used", &mb_used, 2},
-#endif
-    
 #endif
 
 #ifdef LINUX
