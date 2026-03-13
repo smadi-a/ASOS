ef957e23f0e9ca1750968126f9af3562196629f6
Ali Smadi <alielsmadi@gmail.com>
2026-03-13 21:37:00 +1100

    Broaden support in libc

--- user/libc/include/stdio.h
+++ user/libc/include/stdio.h
@@ -11,9 +11,13 @@
 #define EOF (-1)
 
 int printf(const char *fmt, ...) __attribute__((format(printf, 1, 2)));
+int sprintf(char *buf, const char *fmt, ...)
+    __attribute__((format(printf, 2, 3)));
 int snprintf(char *buf, size_t size, const char *fmt, ...)
     __attribute__((format(printf, 3, 4)));
 int vsnprintf(char *buf, size_t size, const char *fmt, va_list ap);
+int vsprintf(char *buf, const char *fmt, va_list ap);
+int sscanf(const char *str, const char *fmt, ...);
 
 int putchar(int c);
 int puts(const char *s);
